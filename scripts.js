295d0a0acd2b7afe7f945e2793ae09b6efc84281
RodrigoDiniz1 <rodrigodinizdigital@gmail.com>
2025-02-27 22:59:52 -0300

    finalização

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("password");
+const jobSelect = document.querySelector("#job");
+const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("submit")
