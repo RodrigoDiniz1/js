9d811824940918c6d6a6139963e571eab7b2d942
RodrigoDiniz1 <rodrigodinizdigital@gmail.com>
2025-02-28 23:19:02 -0300

    email validation

--- scripts.js
+++ scripts.js
@@ -15,4 +15,29 @@ form.addEventListener("submit", (event) => {
         return;
     }
 
+    // verifica se o e-mail está preenchido e se é válido
+    if(emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha o seu e-mail")
+        return
+    }
+
+
+    //Se todos os campos estiverem corretamente preenchidos, envie o form
+
+    form.submit();
 });
+
+function isEmailValid(email) {
+
+//cria uma regex para validar email
+const emailRegex = new RegExp(
+    //
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+);
+
+if(emailRegex.test(email)) {
+    return true;
+}
+
+return false;
+}
